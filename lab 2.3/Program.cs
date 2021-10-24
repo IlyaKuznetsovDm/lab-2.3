1a91fe76db1ff5ba71eec79f8e25a3be81f5ea49
Dmitr <Dmitr@DESKTOP-H2P4N61>
2021-10-24 21:51:50 +0200

    Добавьте файлы проекта.

---
+++ lab 2.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lab_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //2.3 - базовый уровень - вариант 8
+                Console.WriteLine("Введите 1,2 или 3");
+                double n = double.Parse(Console.ReadLine());
+                double a = 0, b = 0, c = 0;
+                switch (n)
+                {
+                    case 1: a = 3; b = 3.5; c = 1.1; break;
+                    case 2: a = 3; b = 6.55; c = 6.55; break;
+                    case 3: a = 0.9; b = 0.9; c = 0.9; break;
+                    default: Console.WriteLine("Нет такого типа"); break;
+                }
+                if ((a == b) || (a == c) || (b == c))
+                    Console.WriteLine("Треуголник равнобедренный");
+                else
+                    Console.WriteLine("Треуголник неравнобедренный");
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введите корректно переменную");
+            }
+        }
+    }
+}
